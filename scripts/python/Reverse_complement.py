4aa959f3b2febdcf495975af339b4379f2539448
Chester Li <zicheng.li@yale.edu>
2025-09-18 16:06:32 -0400

    reorganize

--- scripts/python/Reverse_complement.py
+++
@@ -1,7 +0,0 @@
-def reverse_complement(seq):
-    complement = str.maketrans('ATCGatcg', 'TAGCtagc')
-    return seq.translate(complement)[::-1]
-
-seq1 = "TTAGGG"
-
-print(reverse_complement(seq1))  
